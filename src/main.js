b321ec1eedeb7b5acc097dfd688cc27400591002
KryanCH17 <ryan.koh.2023@scis.smu.edu.sg>
2024-11-10 18:23:30 +0800

    entry file

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './addpost';
+import './apcheck.js';
+import './app.js';
+import './cart.js';
+import './chat.js';
+import './checkout.js';
+import './footer.js';
+import './getMyPost.js';
+import './getMyPurchase.js';
+import './getMySales.js';
+import './getOthersPost.js';
+import './getPost.js';
+import './getreview.js';
+import './navbar.js';
+import './postDetails.js';
+import './postReviews.js';
+
+console.log('Main.js is loaded and bundled!');
