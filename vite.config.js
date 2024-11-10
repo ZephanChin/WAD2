91c588f6f780f04bad35ce9787166aa9726bc4d6
KryanCH17 <ryan.koh.2023@scis.smu.edu.sg>
2024-11-10 18:45:25 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
-  base: './', // Ensures paths are relative to project root
-  plugins: [vue()],
+  base: './',  // Ensures paths are relative to project root
 });
